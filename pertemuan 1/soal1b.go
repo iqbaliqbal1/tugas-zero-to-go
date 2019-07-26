1df2e3c1766c881e70fad3fcc74ca971ca0b39dd
iqbal iqbal <iqbal@alterra.id>
2019-07-26 11:31:55 +0700

    pertemuan1

---
+++ pertemuan 1/soal1b.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main ()  {
+
+	var a int = 20
+	var t int = 25
+	var hasil int = a * t / 2
+		fmt.Println(hasil)
+	
+	
+}
---
+++ pertemuan 1/soal5.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main ()  {
+	var bintang string = "*"
+	var n int = 5 
+	var spasi string = " "
+	fmt.Println(bintang ,n , spasi)
+	
+
+}
